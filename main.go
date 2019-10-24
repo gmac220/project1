84de36dac550f0fd70e3108b3f2e78a35078034c
gmac220 <godfreymacasero@gmail.com>
2019-10-23 21:03:15 -0500

    loop through programs

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"strconv"
+	"os/exec"
 )
 
 func main() {
 	// memStats := new(runtime.MemStats)
 	// runtime.ReadMemStats(memStats)
-	fmt.Println("arc", runtime.GOARCH)
-	fmt.Println("os", runtime.GOOS)
-	fmt.Println("cpus", strconv.Itoa(runtime.NumCPU()))
+	// fmt.Println("arc", runtime.GOARCH)
+	// fmt.Println("os", runtime.GOOS)
+	// fmt.Println("go root", runtime.GOROOT())
+	// fmt.Println("cpus", strconv.Itoa(runtime.NumCPU()))
+	// cdUsr := exec.Command("cd", "..")
+	// cdUsr.Stdin = os.Stdin
+	// _, err := cdUsr.Output()
+	// cdUsr.Output()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	lsUsr := exec.Command("ls", "/usr/bin")
+	lsOutput, stderr := lsUsr.Output()
+	programs := make([]string, 1)
+	var count int = 0
+	if stderr != nil {
+		fmt.Println(stderr)
+	}
+	for i := 0; i < len(lsOutput); i++ {
+		if lsOutput[i] != 10 {
+			programs[count] += string(lsOutput[i])
+		} else {
+			count++
+			programs = append(programs, "")
+		}
+	}
+	fmt.Println(programs)
+	// cdProj1 := exec.Command("cd", "~/go/src/github.com/gmac220/project1")
+	// cdProj1.Run()
 }
